3398cddd6f888dbae1fd918a7ca017098ed25657
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-05-26 12:09:42 -0300

    style(home): alterando estilização da tela, método capitalizeFirstLetter para Capitalizar primeira letra do nome de cada Pokémon

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,19 +6,25 @@ import { HttpService } from '../services/http.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit{
-
-  pokemons: any[] = []
+export class HomePage implements OnInit {
+  pokemons: any[] = [];
 
   constructor(private httpService: HttpService) {}
 
-  ngOnInit(){
-    this.getPokemons()
+  ngOnInit() {
+    this.getPokemons();
   }
 
   getPokemons() {
     this.httpService.getPokemons().subscribe((data: any) => {
-      this.pokemons = data.results;
+      this.pokemons = data.results.map((pokemon: any) => {
+        pokemon.name = this.capitalizeFirstLetter(pokemon.name);
+        return pokemon;
+      });
     });
+  }
+
+  capitalizeFirstLetter(word: string): string {
+    return word.charAt(0).toUpperCase() + word.slice(1);
   }
 }
