a80e63e94790c89d0867810721ba8ee8f7199c56
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-05-26 11:52:22 -0300

    feat(home): Adicionado método getPokemons para buscar dados dos pokemons

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpService } from '../services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  pokemons: any[] = []
 
+  constructor(private httpService: HttpService) {}
+
+  getPokemons() {
+    this.httpService.getPokemons().subscribe((data: any) => {
+      this.pokemons = data.results;
+    });
+  }
 }
